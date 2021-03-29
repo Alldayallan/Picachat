7912065ae474a03a69ff4e623f9efbd4bc60a32e
Allan <mail@allanreid.co>
2021-03-28 19:40:22 -0700

    toolbar touches

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,15 +4,20 @@
       class="bg-white text-grey-10"
       bordered
     >
-      <q-toolbar>
+      <q-toolbar class="constrain">
         <q-btn
           to="/camera"
-          class="large-screen-only"
+          class="large-screen-only q-mr-sm"
           flat
           icon="eva-camera-outline"
           size="18px"
           round
           dense
+        />
+        <q-separator
+          class="large-screen-only"
+          vertical
+          spaced
         />
         <q-toolbar-title
         class="text-grand-hotel text-bold">
@@ -69,9 +74,13 @@ export default {
 </script>
 
 <style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 70px
   .q-toolbar__title
-    text-align: center
     font-size: 30px
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
   .q-footer
     .q-tab__icon
       font-size: 30px
