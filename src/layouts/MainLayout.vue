754844b3601f2a035f9956a17dbfe81abc1be850
Allan <mail@allanreid.co>
2021-03-28 18:44:39 -0700

    first

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+      class="bg-white text-grey-10"
+      bordered
+    >
+      <q-toolbar>
+        <q-btn
+          to="/camera"
+          class="large-screen-only"
+          flat
+          icon="eva-camera-outline"
+          size="18px"
+          round
+          dense
+        />
+        <q-toolbar-title
+        class="text-grand-hotel text-bold">
+          Picachat
+        </q-toolbar-title>
+        <q-btn
+          to="/"
+          class="large-screen-only"
+          flat
+          icon="eva-home-outline"
+          size="18px"
+          round
+          dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer
+      class="bg-white small-screen-only"
+      bordered
+    >
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab
+          to="/"
+          icon="eva-home-outline"
+        />
+        <q-route-tab
+          to="/camera"
+          icon="eva-camera-outline"
+        />
+        </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .q-toolbar__title
+    text-align: center
+    font-size: 30px
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+</style>
